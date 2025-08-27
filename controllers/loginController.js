f10b07e4e7bdf2102ff355b731c3f21d7a5c913f
wjdaksry <xcruisermk@gmail.com>
2025-08-28 00:58:12 +0900

    modify

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -11,7 +11,34 @@ const getLogin = (req,res) => {
 const loginUser = asyncHandler(async(req,res) =>{
     const {username, password } = req.body;
 
-    const user = await User
+    const user = await User.findOne({username});
+    if(!user){
+        return res.json("아이디가 맞지 않습니다.");
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.json("비밀번호가 맞지 않습니다.");
+    }
+
+    const token = jwt.sign({id: user._id}, jwtsecret) //sign으로 토큰생성
+    res.cookie("token", token, {httpOnly: true});
+    res.redirect("/contacts") //로그인 성공시 redirect 경로
 })
 
+
+//회원가입 //POST 경로
+const registerUser = asyncHandler(async(req,res) => {
+    const {username, password, password2} = req.body;
+    if (password === password2){//성공
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({username, password : hashedPassword});
+
+        res.json({message : "회원가입 성공", user});
+        
+    }
+    else{//fail
+        res.send("Failed to Register")
+    }
+})
 
