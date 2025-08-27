81b6dda7a3df34a0992ca1231850a92554ea207f
wjdaksry <xcruisermk@gmail.com>
2025-08-28 01:43:47 +0900

    a

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const User = require('../models/userModel');
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
-//Login Page
+//Login Page 랜더링
 const getLogin = (req,res) => {
-    res.render('home');  //로그인 경로
+    res.render('home');  //home 이름의 ejs
 }
 
 //loginUser
@@ -16,23 +18,28 @@ const loginUser = asyncHandler(async(req,res) =>{
         return res.json("아이디가 맞지 않습니다.");
     }
 
-    const isMatch = bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if(!isMatch){
         return res.json("비밀번호가 맞지 않습니다.");
     }
 
-    const token = jwt.sign({id: user._id}, jwtsecret) //sign으로 토큰생성
+    const token = jwt.sign({id: user._id}, jwtSecret) //sign으로 토큰생성
     res.cookie("token", token, {httpOnly: true});
     res.redirect("/contacts") //로그인 성공시 redirect 경로
 })
 
 
+//회원가입 랜더링
+const getRegister = (req,res) =>{
+    res.render("register"); //register 이름의 ejs
+}
+
 //회원가입 //POST 경로
-const registerUser = asyncHandler(async(req,res) => {
+const registerUser = asyncHandler(async(req,res) => { 
     const {username, password, password2} = req.body;
-    if (password === password2){//성공
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({username, password : hashedPassword});
+    if (password === password2){ //비밀번호 확인 로직
+        const hashedPassword = await bcrypt.hash(password, 10);  //해싱
+        const user = await User.create({username, password : hashedPassword}); //db에 저장
 
         res.json({message : "회원가입 성공", user});
         
@@ -42,3 +49,5 @@ const registerUser = asyncHandler(async(req,res) => {
     }
 })
 
+
+module.exports= {getLogin, loginUser, getRegister, registerUser};
